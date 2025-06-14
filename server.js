7d2136b87287bf41d33a2b496681b7b1d14b26f6
Mayuresh Madav <114469787+MayureshDMadav@users.noreply.github.com>
2025-06-14 10:19:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
+app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
